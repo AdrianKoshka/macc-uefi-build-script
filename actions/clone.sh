f557bf1f6f0e2734ccd2aaf60c66cfc0e9819c33
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2020-11-06 22:35:52 -0500

    Prep for github actions

---
+++ actions/clone.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+BDIR=e2b
+mkdir $BDIR
+cd $BDIR
+
+git clone --recurse-submodules https://github.com/tianocore/edk2.git -b edk2-stable202008
+git clone https://github.com/tianocore/edk2-non-osi.git -b master
+git clone https://github.com/tianocore/edk2-platforms.git -b master
+cd edk2-platforms
+git checkout 1e09147a01aeb45aa43e36923d96a1d6b0ec9106
+cd ..
+git clone https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git -b master
+git clone https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-18.12 binaries
+git clone https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git -b mv-ddr-devel
---
+++ actions/tfa.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+BDIR=e2b
+
+cd ${BDIR}
+export PYTHON_COMMAND=/usr/bin/python3.8
+export export WORKSPACE=$PWD
+export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/edk2-non-osi
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+make -C trusted-firmware-a \
+        PLAT=a80x0_mcbin \
+        MV_DDR_PATH=$PWD/mv-ddr-marvell \
+        SCP_BL2=$PWD/binaries/mrvl_scp_bl2.img \
+        BL33=$PWD/Build/Armada80x0McBin-AARCH64/RELEASE_GCC5/FV/ARMADA_EFI.fd \
+        all fip
+
+cp trusted-firmware-a/build/a80x0_mcbin/release/fip.bin uefi-mcbin-spi.bin
