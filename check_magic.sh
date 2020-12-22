37790832c283811df477a75d734159b48ad00813
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2020-12-22 17:52:21 -0500

    Fix file path

--- check_magic.sh
+++ check_magic.sh
@@ -1,4 +1,4 @@
-magic_number=`xxd e2b/trusted-firmware-a/build/a80x0_mcbin/release/fip.bin | head -n1 | grep -o "0100 64aa"`
+magic_number=`xxd actions/e2b/trusted-firmware-a/build/a80x0_mcbin/release/fip.bin | head -n1 | grep -o "0100 64aa"`
 if [ "$magic_number" != "0100 64aa" ]; then
   echo "Correct number"
 fi
