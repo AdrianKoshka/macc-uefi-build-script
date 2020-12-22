4148ecd844912d9e80e89f80abc22f5fcff739f3
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2020-12-22 17:44:34 -0500

    Add a check for UEFI magic numbers

---
+++ check_magic.sh
@@ -0,0 +1,4 @@
+magic_number=`xxd e2b/trusted-firmware-a/build/a80x0_mcbin/release/fip.bin | head -n1 | grep -o "0100 64aa"`
+if [ "$magic_number" != "0100 64aa" ]; then
+  echo "Correct number"
+fi
