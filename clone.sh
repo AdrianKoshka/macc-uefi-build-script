3278ff3c19f6c333e2d9d7e27f6a84a4f5662785
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-05-22 03:46:06 -0400

    Add missing parameter to make header magic work

--- clone.sh
+++ clone.sh
@@ -7,5 +7,9 @@ git clone --recurse-submodules https://github.com/tianocore/edk2.git -b master
 git clone https://github.com/tianocore/edk2-non-osi.git -b master
 git clone https://github.com/tianocore/edk2-platforms.git -b master
 git clone https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git -b master
+cd trusted-firmware-a
+cp ../../tfa.patch tfa.patch
+git apply tfa.patch
+cd ..
 git clone https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-18.12 binaries
 git clone https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git -b mv-ddr-devel
