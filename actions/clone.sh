afee2d6d7e1d967f26cb7d9975cc0630410d4b34
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-05-29 14:37:02 -0400

    Update branch to clone from for marvell binaries

--- actions/clone.sh
+++ actions/clone.sh
@@ -11,5 +11,5 @@ cd trusted-firmware-a
 cp ../../tfa.patch tfa.patch
 git apply tfa.patch
 cd ..
-git clone https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-18.12 binaries
+git clone https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-SDK10.0.1.0 binaries
 git clone https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git -b mv-ddr-devel
