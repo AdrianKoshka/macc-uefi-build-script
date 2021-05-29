bf09442786247283b477cb614bef3a603d9f968b
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2021-05-29 14:37:36 -0400

    Add more variables from the official macchiatobin wiki

--- actions/tfa.sh
+++ actions/tfa.sh
@@ -4,9 +4,12 @@ BDIR=e2b
 cd ${BDIR}
 export export WORKSPACE=$PWD
 export PACKAGES_PATH=$PWD/edk2:$PWD/edk2-platforms:$PWD/edk2-non-osi
+export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
 make -C trusted-firmware-a \
+        USE_COHERENT_MEM=0 \
+        LOG_LEVEL=20 \
         PLAT=a80x0_mcbin \
         MV_DDR_PATH=$PWD/mv-ddr-marvell \
         SCP_BL2=$PWD/binaries/mrvl_scp_bl2.img \
