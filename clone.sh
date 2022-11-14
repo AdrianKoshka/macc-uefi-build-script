1d91847023a34b64cc276f63de3aa312ace01dd4
Adrian Lucrèce Céleste <adrian.lucrece.celeste@ibm.com>
2022-11-14 17:03:37 -0500

    Add shallow cloning and gitignore

--- clone.sh
+++ clone.sh
@@ -3,13 +3,13 @@ BDIR=e2b
 mkdir $BDIR
 cd $BDIR
 
-git clone --recurse-submodules https://github.com/tianocore/edk2.git -b master
-git clone https://github.com/tianocore/edk2-non-osi.git -b master
-git clone https://github.com/tianocore/edk2-platforms.git -b master
-git clone https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git -b master
+git clone --depth=1 --recurse-submodules https://github.com/tianocore/edk2.git -b master
+git clone --depth=1 https://github.com/tianocore/edk2-non-osi.git -b master
+git clone --depth=1 https://github.com/tianocore/edk2-platforms.git -b master
+git clone --depth=1 https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git -b master
 cd trusted-firmware-a
 cp ../../tfa.patch tfa.patch
 git apply tfa.patch
 cd ..
-git clone https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-SDK10.0.1.0 binaries
-git clone https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git -b master
+git clone --depth=1 https://github.com/MarvellEmbeddedProcessors/binaries-marvell.git -b binaries-marvell-armada-SDK10.0.1.0 binaries
+git clone --depth=1 https://github.com/MarvellEmbeddedProcessors/mv-ddr-marvell.git -b master
